4ee354bb2c7cedce10b70889ced8f890c9c4eb0f
Facundo Paez <facundopaez1998@gmail.com>
2022-05-03 00:26:50 -0300

    Add Black button and Rainbow button functionality

--- script.js
+++ script.js
@@ -1,43 +1,83 @@
 const container = document.querySelector('.container');
-const clearButton = document.getElementById('clear-button');
+const blackButton = document.getElementById('black-button');
+const rainbowButton = document.getElementById('rainbow-button');
 const sizeButton = document.getElementById('size-button');
+const clearButton = document.getElementById('clear-button');
+
 let currentSize = 16;
 
-createGrid(16)
+createGrid(currentSize);
 
-function createGrid(gridSize){
+function createGrid(gridSize) {
     const totalBoxes = gridSize * gridSize;
-    
+
     container.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
 
-    for(let i = 0; i < totalBoxes; i++){
+    for (let i = 0; i < totalBoxes; i++) {
         let box = document.createElement('div');
         box.addEventListener('mouseover', () => box.className = 'hover');
         container.appendChild(box);
     }
 }
 
-function clearGrid(){
-    while(container.firstChild){
+function clearGrid() {
+    while (container.firstChild) {
         container.removeChild(container.lastChild);
     }
 }
 
-clearButton.addEventListener('click', function() {
-    clearGrid();
-    createGrid(currentSize);
+// Generates a random value between 0 and 255
+function randomNum() {
+    return Math.floor(Math.random() * 256);
+}
+
+// Returns a string of a rgb value
+function randomRGB() {
+    let red = randomNum();
+    let green = randomNum();
+    let blue = randomNum();
+    return "rgb(" + red + ", " + green + ", " + blue + ")";
+}
+
+function changeHoverToBlack() {
+    const boxes = container.childNodes;
+
+    boxes.forEach(box => {
+        box.addEventListener('mouseover', () => box.style.backgroundColor = 'black');
+    });
+}
+
+function changeHoverToRandom() {
+    const boxes = container.childNodes;
+
+    boxes.forEach(box => {
+        box.addEventListener('mouseover', () => box.style.backgroundColor = randomRGB());
+    });
+}
+
+blackButton.addEventListener('click', function () {
+    changeHoverToBlack();
 });
 
-sizeButton.addEventListener('click', function() {
+rainbowButton.addEventListener('click', function() {
+    changeHoverToRandom();
+})
+
+sizeButton.addEventListener('click', function () {
     const newSize = prompt('Choose the size of the new grid (between 1 and 100)');
 
-    if(newSize === null) return;
+    if (newSize === null) return;
 
-    if(Number.isNaN(newSize) || newSize < 1 || newSize > 100) {
+    if (Number.isNaN(newSize) || newSize < 1 || newSize > 100) {
         alert('Please enter a number between 1 and 100');
     } else {
         clearGrid();
         createGrid(newSize);
         currentSize = newSize;
     }
+});
+
+clearButton.addEventListener('click', function () {
+    clearGrid();
+    createGrid(currentSize);
 });
